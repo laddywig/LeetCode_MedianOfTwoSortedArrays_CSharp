6e74540bee962fc275a36a67a111e0903b05eccc
Christopher Ladewig <christopher.ladewig@avanade.com>
2023-04-01 12:35:09 -0500

    Add project files.

---
+++ LeetCode_MedianOfTwoSortedArrays_CSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode_MedianOfTwoSortedArrays_CSharp;
+
+Solution classObject = new();
+Console.WriteLine(classObject.FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }).ToString("0.00000"));
+Console.WriteLine(classObject.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }).ToString("0.00000"));
+Console.WriteLine(classObject.FindMedianSortedArrays(new int[] { }, new int[] { 1 }).ToString("0.00000"));
+Console.WriteLine(classObject.FindMedianSortedArrays(new int[] { }, new int[] { 2, 3 }).ToString("0.00000"));
+Console.WriteLine(classObject.FindMedianSortedArrays(new int[] { 2, 2, 4, 4 }, new int[] { 2, 2, 4, 4 }).ToString("0.00000"));
+Console.WriteLine(classObject.FindMedianSortedArrays(new int[] { 1 }, new int[] { 1 }).ToString("0.00000"));
+Console.WriteLine(classObject.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 1, 2, 3 }).ToString("0.00000"));
---
+++ LeetCode_MedianOfTwoSortedArrays_CSharp/Solution.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_MedianOfTwoSortedArrays_CSharp
+{
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int sum = nums1.Length + nums2.Length;
+            int num1tracker = 0;
+            int num2tracker = 0;
+            double lastNum = 0;
+            bool isEven = sum % 2 == 0;
+            bool willBreak = false;
+            int count = GetCombinedSizeHalf(sum);
+            int lastNumTracker = isEven ? count - 2 : count - 1;
+            for (int i = 0; i < count; i++)
+            {
+                if(nums1.Length <= num1tracker)
+                {
+                    if (i >= lastNumTracker)
+                    {
+                        lastNum += nums2[num2tracker];
+                    }
+                    num2tracker++;
+                }
+                else if (nums2.Length <= num2tracker)
+                {
+                    if (i >= lastNumTracker)
+                    {
+                        lastNum += nums1[num1tracker];
+                    }
+                    num1tracker++;
+                }
+                else if (nums1[num1tracker] < nums2[num2tracker])
+                {
+                    if (i >= lastNumTracker)
+                    {
+                        lastNum += nums1[num1tracker];
+                    }
+                    num1tracker++;
+                }
+                else if (nums2[num2tracker] < nums1[num1tracker])
+                {
+                    if (i >= lastNumTracker)
+                    {
+                        lastNum += nums2[num2tracker];
+                    }
+                    num2tracker++;
+                }
+                else if (nums2[num2tracker] == nums1[num1tracker])
+                {
+                    if (i >= lastNumTracker)
+                    {
+                        if (!isEven || lastNum != 0)
+                        {
+                            willBreak = true;
+                        }
+                        lastNum += nums2[num2tracker];
+
+                        if (willBreak)
+                        {
+                            break;
+                        }
+                    }
+                    i++;
+                    if (i >= lastNumTracker)
+                    {
+                        lastNum += nums2[num2tracker];
+                    }
+                    num1tracker++;
+                    num2tracker++;
+                }
+                else
+                {
+                    throw new ApplicationException("Logic should never execute this line of code");
+                }
+            }
+
+            if (isEven)
+            {
+                return (lastNum) / 2.0;
+            }
+            else
+            {
+                return Convert.ToDouble(lastNum);
+            }
+        }
+
+        public int GetCombinedSizeHalf(int sum) 
+        {
+            if (sum % 2 == 0)
+            {
+                return (sum / 2)+1;
+            }
+            else
+            {
+                return (sum + 1) / 2;
+            }
+        }
+    }
+}
